f746a7095ae9f69acf59e0a31ae25c54efc292b2
Spooghetti420 <kkovachev1947@gmail.com>
2022-01-30 20:41:52 +0000

    Update script tag generator

--- generate_script_tags.py
+++ generate_script_tags.py
@@ -1,6 +1,17 @@
+"""
+This script is intended to add all necessary JavaScript dependencies from the source tree into the
+`index.html` file. It simply regexes out all of the existing script tags and replaces them with the necessary ones.
+The reason it checks the `src` directory and goes to the trouble of replacing `src` with `dist` and `.ts` with 
+`.js` is that the `dist` directory can become bloated with old files over time, and deleting them can be a pain
+without the right automation. This is the easiest way to only copy the files that'll be used in the build
+and not worry about junk JavaScript files. 
+"""
+
 import re
 import os
+import subprocess
 
+subprocess.run(["tsc"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) # Ensure project is compiled
 new_content = ""
 with open("index.html", encoding="utf-8", mode="r") as index:
     content = index.read()
